ebbebcf700bd03a98c35d27d4e1a1d15d3f74c01
包涵 <1004578954@qq.com>
2020-07-31 14:33:46 +0800

    a

---
+++ Scripts/Runtime/MenuButtonLV2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.SimuUI
+{
+    [RequireComponent(typeof(Button))]
+    public class MenuButtonLV2 : MonoBehaviour
+    {
+        Button button;
+        void Start()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(() =>
+            {
+                PanelTools.Instance.CloseAllMenu();
+            });
+        }
+    }
+}
